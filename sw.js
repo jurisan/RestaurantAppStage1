0964ec9ef2e55fabc5fd69464e5e1514d432d94d
Julie-Nakadai <julienakadai@icloud.com>
2018-10-13 18:06:37 -0400

    commit the rest

---
+++ sw.js
@@ -0,0 +1,57 @@
+//declare scope for Service Worker
+const currentCache = 'restaurantsCachev1';
+const urlsToCache = [
+  '/',
+  'restaurant.html',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js'
+];
+
+//Installing Service Worker
+
+self.addEventListener('install', function(event)){
+        event.waitUntil(
+            caches.open(currentCache).then (function(cache){
+                return cache.addAll(urlsToCache);
+        })
+    );
+}); 
+
+//Activate Service Worker
+
+self.addEventListener('activate', function(event){
+        event.waitUntil(
+            caches.keys().then(function(cacheNames){
+                return Promise.all(
+                    cacheNames.filter(function(cacheName) {
+                        return cacheName ! = currentCache;
+                    }).map(function(cacheName) {
+                        return caches.delete(cacheName);
+                    })
+                );
+            })
+        );
+});
+
+//Fetching for offline content 
+
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    ); 
+});
